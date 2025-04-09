8acc9eef872a1da64b583ab1dcefd5b42b39259b
Aidar <abdzhaid@cvut.cz>
2025-04-09 20:03:48 +0200

    Collision class was added

---
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/Collision.java
@@ -0,0 +1,24 @@
+package cvut.cz;
+
+public class Collision {
+    private int worldCoordinateX;
+    private int worldCoordinateY;
+    private int width;
+    private int height;
+
+    public Collision(int worldCoordinateX, int worldCoordinateY, int width, int height) {
+        this.worldCoordinateX = worldCoordinateX;
+        this.worldCoordinateY = worldCoordinateY;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWorldCoordinateX() { return worldCoordinateX; }
+    public int getWorldCoordinateY() { return worldCoordinateY; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+    public void setWorldCoordinateX(int worldCoordinateX) { this.worldCoordinateX = worldCoordinateX; }
+    public void setWorldCoordinateY(int worldCoordinateY) { this.worldCoordinateY = worldCoordinateY; }
+    public void setWidth(int width) { this.width = width; }
+    public void setHeight(int height) { this.height = height; }
+}
