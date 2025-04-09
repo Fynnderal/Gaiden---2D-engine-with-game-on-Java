bbf3f82bd8d6d49d28cb72ce2a3d953909afa46e
Aidar <abdzhaid@cvut.cz>
2025-04-09 20:06:49 +0200

    Tile class added

---
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/Tile.java
@@ -0,0 +1,16 @@
+package cvut.cz;
+
+import java.util.logging.Logger;
+import java.net.URL;
+
+public class Tile extends GameSprite {
+    private static int sourceTileSize;
+    public Tile(URL pathToImage, int sourceX, int sourceY, int sourceTileSize) {
+        super(pathToImage, sourceX, sourceY, sourceTileSize, sourceTileSize, 0, 0, sourceTileSize, sourceTileSize, 0, 0);
+        Tile.sourceTileSize = sourceTileSize;
+    }
+
+    public void setWorldCoordinateX(int worldCoordinateX) { this.worldCoordinateX = worldCoordinateX; }
+    public void setWorldCoordinateY(int worldCoordinateY) { this.worldCoordinateY = worldCoordinateY; }
+    public static int getSourceTileSize() {  return Tile.sourceTileSize; }
+}
