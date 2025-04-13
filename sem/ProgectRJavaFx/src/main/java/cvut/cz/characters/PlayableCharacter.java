53539d63fc28093614edf26e10cb4b98bfd1f26d
Aidar <abdzhaid@cvut.cz>
2025-04-13 18:30:09 +0200

    PlayableCharacter: Methids readAvailableItems and writeAvailableItems were added

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/characters/PlayableCharacter.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/characters/PlayableCharacter.java
@@ -1,18 +1,39 @@
 package cvut.cz.characters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cvut.cz.items.Item;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.logging.Logger;
 import java.net.URL;
 
 
 public abstract class PlayableCharacter extends GameCharacter{
     private static final Logger logger = Logger.getLogger(PlayableCharacter.class.getName());
+    private final static ObjectMapper mapper = new ObjectMapper();
+//    private final static SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("itemFilter", SimpleBeanPropertyFilter.filterOutAllExcept("Name", "Broken", "Amount"));
+
+    protected Item[] items;
+    private URL pathToItems;
+
+
+
+    public void setItems(Item[] items) { this.items = items; }
+
+    public Item[] getItems() {return items;}
 
     public PlayableCharacter(URL pathToInventory, URL pathToImage, int sourceCoordinateX, int sourceCoordinateY, int sourceWidth, int sourceHeight, int screenCoordinateX, int screenCoordinateY, int targetWidth, int targetHeight, int worldCoordinateX, int worldCoordinateY ) {
-        super(pathToImage, sourceCoordinateX, sourceCoordinateY, sourceWidth, sourceHeight, screenCoordinateX, screenCoordinateY, targetWidth, targetHeight, worldCoordinateX, worldCoordinateY);
+        this(0, States.IDLE, 0, 0, 0,pathToInventory, pathToImage, sourceCoordinateX, sourceCoordinateY, sourceWidth, sourceHeight, screenCoordinateX, screenCoordinateY, targetWidth, targetHeight, worldCoordinateX, worldCoordinateY);
     }
 
-    public PlayableCharacter(int attackPower, States currentState, int currentHealth, int maxHealth, double speed, URL pathToImage, int sourceCoordinateX, int sourceCoordinateY, int sourceWidth, int sourceHeight, int screenCoordinateX, int screenCoordinateY, int targetWidth, int targetHeight, int worldCoordinateX, int worldCoordinateY) {
+    public PlayableCharacter(int attackPower, States currentState, int currentHealth, int maxHealth, double speed, URL pathToItems, URL pathToImage, int sourceCoordinateX, int sourceCoordinateY, int sourceWidth, int sourceHeight, int screenCoordinateX, int screenCoordinateY, int targetWidth, int targetHeight, int worldCoordinateX, int worldCoordinateY) {
         super(attackPower, currentState, currentHealth, maxHealth, speed, pathToImage, sourceCoordinateX, sourceCoordinateY, sourceWidth, sourceHeight, screenCoordinateX, screenCoordinateY, targetWidth, targetHeight, worldCoordinateX, worldCoordinateY);
+        this.pathToItems = pathToItems;
+        if (pathToItems != null) {
+            readAvailableItems(this.pathToItems);
+        }
     }
 
     public void Move(Directions direction) {
@@ -32,5 +53,24 @@ public abstract class PlayableCharacter extends GameCharacter{
                 break;
         }
         currentState = States.IDLE;
+    }
+
+
+    public void readAvailableItems(URL fileName) {
+        try (FileReader fileReader = new FileReader(fileName.getPath())) {
+            items = mapper.readValue(fileReader, Item[].class);
+        }
+        catch (IOException e) {
+            System.err.println("[ERROR] Problem with reading json file. Problem: " + e.getMessage());
+        }
+
+    }
+    public void writeAvailableItems(URL fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName.getPath())) {
+            mapper.writeValue(fileWriter, items);
+        }
+        catch (IOException e) {
+            System.err.println("[ERROR] Problem with writing into json file. Problem: " + e.getMessage());
+        }
     }
 }
