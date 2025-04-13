3d29abad87d37fc9f2bc879231168caf4c9dae60
Aidar <abdzhaid@cvut.cz>
2025-04-13 18:30:50 +0200

    URL pathToItems was added to constructor in OfficeWorker

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/characters/OfficeWorker.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/characters/OfficeWorker.java
@@ -3,12 +3,12 @@ package cvut.cz.characters;
 import java.net.URL;
 
 public class OfficeWorker extends PlayableCharacter{
-    public OfficeWorker(URL pathToImage, int sourceCoordinateX, int sourceCoordinateY, int sourceWidth, int sourceHeight, int targetCoordinateX, int targetCoordinateY, int targetWidth, int targetHeight, int worldCoordinateX, int worldCoordinateY){
-        this(0, States.IDLE, 0, 0, 0, pathToImage, sourceCoordinateX, sourceCoordinateY, sourceWidth, sourceHeight, targetCoordinateX, targetCoordinateY, targetWidth, targetHeight, worldCoordinateX, worldCoordinateY);
+    public OfficeWorker(URL pathToItems, URL pathToImage, int sourceCoordinateX, int sourceCoordinateY, int sourceWidth, int sourceHeight, int targetCoordinateX, int targetCoordinateY, int targetWidth, int targetHeight, int worldCoordinateX, int worldCoordinateY){
+        this(0, States.IDLE, 0, 0, 0, pathToItems, pathToImage, sourceCoordinateX, sourceCoordinateY, sourceWidth, sourceHeight, targetCoordinateX, targetCoordinateY, targetWidth, targetHeight, worldCoordinateX, worldCoordinateY);
     }
 
-    public OfficeWorker(int attackPower, States currentState, int currentHealth, int maxHealth, double speed, URL pathToImage, int sourceCoordinateX, int sourceCoordinateY, int sourceWidth, int sourceHeight, int targetCoordinateX, int targetCoordinateY, int targetWidth, int targetHeight, int worldCoordinateX, int worldCoordinateY) {
-        super(attackPower, currentState, currentHealth, maxHealth, speed, pathToImage, sourceCoordinateX, sourceCoordinateY, sourceWidth, sourceHeight, targetCoordinateX, targetCoordinateY, targetWidth, targetHeight, worldCoordinateX, worldCoordinateY);
+    public OfficeWorker(int attackPower, States currentState, int currentHealth, int maxHealth, double speed, URL pathToItems, URL pathToImage, int sourceCoordinateX, int sourceCoordinateY, int sourceWidth, int sourceHeight, int targetCoordinateX, int targetCoordinateY, int targetWidth, int targetHeight, int worldCoordinateX, int worldCoordinateY) {
+        super(attackPower, currentState, currentHealth, maxHealth, speed, pathToItems, pathToImage, sourceCoordinateX, sourceCoordinateY, sourceWidth, sourceHeight, targetCoordinateX, targetCoordinateY, targetWidth, targetHeight, worldCoordinateX, worldCoordinateY);
     }
     @Override
     protected void Attack() {
