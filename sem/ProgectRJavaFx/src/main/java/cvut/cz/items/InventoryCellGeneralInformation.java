d69b904296d1c07b45d23bab79061b314e06065e
Aidar <abdzhaid@cvut.cz>
2025-04-15 23:16:41 +0200

    Class InventoryCellInformation was changed to record InventoryCellGeneralInformation

---
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/items/InventoryCellGeneralInformation.java
@@ -0,0 +1,4 @@
+package cvut.cz.items;
+
+public record InventoryCellGeneralInformation(int cellWidth, int cellHeight, int itemCoordinateXRelativeToCell, int itemCoordinateYRelativeToCell, int itemWidthInCell, int itemHeightInCell, int amountLabelCoordinateXRelativeToCell, int amountLabelCoordinateYRelativeToCell) {
+}
---
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/items/InventoryInformation.java
@@ -0,0 +1,13 @@
+package cvut.cz.items;
+
+import java.util.Map;
+
+public record InventoryInformation (InventoryCellGeneralInformation inventoryCellGeneralInformation, Pointer pointer, Map<String, ItemInformation> possibleItems, int gapBetweenCellsX, int gapBetweenCellsY, int numberOfCells, int numberOfCellsInRow, int firstCellCoordinateX, int firstCellCoordinateY) {
+
+    public InventoryInformation{
+        pointer.getGameSpriteRenderInformation().setScreenCoordinateX(firstCellCoordinateX);
+        pointer.getGameSpriteRenderInformation().setScreenCoordinateY(firstCellCoordinateY);
+        pointer.getGameSpriteRenderInformation().setTargetWidth(inventoryCellGeneralInformation.cellWidth());
+        pointer.getGameSpriteRenderInformation().setTargetHeight(inventoryCellGeneralInformation.cellHeight());
+    }
+}
