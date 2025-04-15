e340465ea8704e044eebc6539674a2d531cc8da0
Aidar <abdzhaid@cvut.cz>
2025-04-15 23:17:38 +0200

    Class itemInformation was transformed to record

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/items/ItemInformation.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/items/ItemInformation.java
@@ -1,48 +1,15 @@
 package cvut.cz.items;
 
-import java.net.URL;
+
+import cvut.cz.GameSpriteRenderInformation;
+import cvut.cz.GameSpriteSourceInformation;
+
 import java.util.Map;
 
-public class ItemInformation {
-    private int sourceX;
-    private int sourceY;
-    private int sourceWidth;
-    private int sourceHeight;
-    private String name;
-    private URL image;
-    private boolean isBroken;
-    private Map<String, String> canBeCombinedWithInto;
-    boolean canBeEquipped;
-    boolean canBeUsed;
-    boolean canBeDiscarded;
-    int standardAmount;
+public record ItemInformation(GameSpriteSourceInformation gameSpriteSourceInformation, GameSpriteRenderInformation gameSpriteRenderInformation, String name, Map<String, String> canBeCombinedWithInto, boolean canBeEquipped, boolean canBeUsed, boolean canBeDiscarded) {
 
-    public ItemInformation(int sourceX, int sourceY, int sourceWidth, int sourceHeight, String name, URL image, boolean isBroken, Map<String, String> canBeCombinedWithInto, boolean canBeEquipped, boolean canBeUsed, boolean canBeDiscarded, int standardAmount) {
-        this.name = name;
-        this.image = image;
-        this.isBroken = isBroken;
-        this.canBeCombinedWithInto = canBeCombinedWithInto;
-        this.canBeEquipped = canBeEquipped;
-        this.canBeUsed = canBeUsed;
-        this.canBeDiscarded = canBeDiscarded;
-        this.sourceX = sourceX;
-        this.sourceY = sourceY;
-        this.sourceWidth = sourceWidth;
-        this.sourceHeight = sourceHeight;
-        this.standardAmount = standardAmount;
-
+    ItemInformation(GameSpriteSourceInformation gameSpriteSourceInformation, String name, Map<String, String> canBeCombinedWithInto, boolean canBeEquipped, boolean canBeUsed, boolean canBeDiscarded) {
+        this(gameSpriteSourceInformation, new GameSpriteRenderInformation(0, 0, 0, 0,0, 0),  name, canBeCombinedWithInto,  canBeEquipped,  canBeUsed,  canBeDiscarded);
     }
 
-    public String getName() { return name; }
-    public URL getImage() { return image; }
-    public boolean isBroken() { return isBroken; }
-    public Map<String, String> getCanBeCombinedWithInto() { return canBeCombinedWithInto; }
-    public boolean isCanBeEquipped() { return canBeEquipped; }
-    public boolean isCanBeUsed() { return canBeUsed; }
-    public boolean isCanBeDiscarded() { return canBeDiscarded; }
-    public int getSourceX() { return sourceX; }
-    public int getSourceY() { return sourceY; }
-    public int getSourceWidth() { return sourceWidth; }
-    public int getSourceHeight() { return sourceHeight; }
-    public int getStandardAmount() { return standardAmount; }
 }
