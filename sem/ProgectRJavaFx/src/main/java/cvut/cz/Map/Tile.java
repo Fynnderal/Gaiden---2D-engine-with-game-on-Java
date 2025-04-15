789c875bb16a4b082978129b365316d64f74544a
Aidar <abdzhaid@cvut.cz>
2025-04-15 23:18:34 +0200

    Tile constructor was changed

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/Map/Tile.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/Map/Tile.java
@@ -1,17 +1,18 @@
 package cvut.cz.Map;
 
 import cvut.cz.GameSprite;
+import cvut.cz.GameSpriteRenderInformation;
+import cvut.cz.GameSpriteSourceInformation;
 
 import java.net.URL;
 
 public class Tile extends GameSprite {
     private static int sourceTileSize;
+
     public Tile(URL pathToImage, int sourceX, int sourceY, int sourceTileSize) {
-        super(pathToImage, sourceX, sourceY, sourceTileSize, sourceTileSize, 0, 0, sourceTileSize, sourceTileSize, 0, 0);
+        super(new GameSpriteSourceInformation(pathToImage, sourceX, sourceY, sourceTileSize, sourceTileSize), new GameSpriteRenderInformation(0, 0, sourceTileSize, sourceTileSize, 0,0 ));
         Tile.sourceTileSize = sourceTileSize;
     }
 
-    public void setWorldCoordinateX(int worldCoordinateX) { this.worldCoordinateX = worldCoordinateX; }
-    public void setWorldCoordinateY(int worldCoordinateY) { this.worldCoordinateY = worldCoordinateY; }
     public static int getSourceTileSize() {  return Tile.sourceTileSize; }
 }
