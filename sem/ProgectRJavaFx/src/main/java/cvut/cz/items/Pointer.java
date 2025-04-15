ec93c15df2ccc966dc98a5d7765c11b0e04cc10a
Aidar <abdzhaid@cvut.cz>
2025-04-15 23:13:49 +0200

    Pointer constructor was changed

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/items/Pointer.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/items/Pointer.java
@@ -1,22 +1,19 @@
 package cvut.cz.items;
 
 import cvut.cz.GameSprite;
+import cvut.cz.GameSpriteRenderInformation;
+import cvut.cz.GameSpriteSourceInformation;
 
 import java.net.URL;
 
 public class Pointer extends GameSprite {
 
-    public Pointer(URL pathToImage, int sourceCoordinateX, int sourceCoordinateY, int sourceWidth, int sourceHeight){
-        super(pathToImage, sourceCoordinateX, sourceCoordinateY, sourceWidth, sourceHeight, 0, 0, 0, 0, 0, 0);
+    public Pointer(GameSpriteSourceInformation gameSpriteSourceInformation){
+        super(gameSpriteSourceInformation, new GameSpriteRenderInformation(0, 0, 0, 0, 0, 0));
     }
 
     public void move(int offsetX, int offsetY){
-        this.screenCoordinateX += offsetX;
-        this.screenCoordinateY += offsetY;
+        this.gameSpriteRenderInformation.setScreenCoordinateX(gameSpriteRenderInformation.getScreenCoordinateX() + offsetX);
+        this.gameSpriteRenderInformation.setScreenCoordinateY(gameSpriteRenderInformation.getScreenCoordinateY() + offsetY);
     }
-
-    public void setScreenCoordinateX(int screenCoordinateX){ this.screenCoordinateX = screenCoordinateX; }
-    public void setScreenCoordinateY(int screenCoordinateY) { this.screenCoordinateY = screenCoordinateY; }
-    public void setTargetWidth(int targetWidth) { this.targetWidth = targetWidth; }
-    public void setTargetHeight(int targetHeight) { this.targetHeight = targetHeight; }
 }
