e19b5c4c9f7ced26f876f7b2a1049b0682e3a84f
Aidar <abdzhaid@cvut.cz>
2025-05-03 22:34:10 +0200

    Javadoc to AnimationInformation was added

---
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/Animation/CharacterAnimation.java
@@ -0,0 +1,36 @@
+package cvut.cz.Animation;
+
+public class CharacterAnimation {
+    public AnimationStates previousAnimationState;
+    public AnimationStates currentAnimationState;
+    public int currentAnimation;
+
+
+    public CharacterAnimation(){
+        previousAnimationState = null;
+        currentAnimationState = AnimationStates.IdleDOWN;
+        currentAnimation = 0;
+    }
+
+    public AnimationInformation[] idleUpAnimation;
+    public AnimationInformation[] idleDownAnimation;
+    public AnimationInformation[] idleLeftAnimation;
+    public AnimationInformation[] idleRightAnimation;
+
+    public AnimationInformation[] moveRightAnimation;
+    public AnimationInformation[] moveLeftAnimation;
+    public AnimationInformation[] moveUpAnimation;
+    public AnimationInformation[] moveDownAnimation;
+
+    public AnimationInformation[] dyingUpAnimation;
+    public AnimationInformation[] dyingDownAnimation;
+    public AnimationInformation[] dyingRightAnimation;
+    public AnimationInformation[] dyingLeftAnimation;
+
+    public AnimationInformation[] takingDamageUpAnimation;
+    public AnimationInformation[] takingDamageDownAnimation;
+    public AnimationInformation[] takingDamageLeftAnimation;
+    public AnimationInformation[] takingDamageRightAnimation;
+
+
+}
