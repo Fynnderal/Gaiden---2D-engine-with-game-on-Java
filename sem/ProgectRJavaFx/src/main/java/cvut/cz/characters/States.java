f1a714fe6f2fb21acd631d706a89c5ce06e94bc0
Aidar <abdzhaid@cvut.cz>
2025-05-11 00:05:17 +0200

    Javadoc to States was added

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/characters/States.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/characters/States.java
@@ -1,9 +1,12 @@
 package cvut.cz.characters;
 
+/**
+ * Represents the various states a character can be in during the game.
+ * These states are used to define the behavior and actions of characters.
+ */
 public enum States {
     IDLE,
     ALARMED,
-    DAMAGED,
     ATTACKING,
     WALKING,
     CHASING
