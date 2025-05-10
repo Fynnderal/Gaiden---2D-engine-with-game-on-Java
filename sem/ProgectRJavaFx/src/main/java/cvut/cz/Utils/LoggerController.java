4c4435cc987b4f32d6882b3140b7957889988153
Aidar <abdzhaid@cvut.cz>
2025-05-10 23:59:01 +0200

    Javadoc to LoggerController was added

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/Utils/LoggerController.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/Utils/LoggerController.java
@@ -3,9 +3,15 @@ package cvut.cz.Utils;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Utility class for controlling the logging behavior of the application.
+ */
 public class LoggerController {
     private LoggerController() {}
 
+    /**
+     * Disables all logging by setting the root logger's level to OFF.
+     */
     public static void offLoggers() {
         Logger rootLogger = Logger.getLogger("");
         rootLogger.setLevel(Level.OFF);
