6bb995a38d4ac6dc099b4d943b4481b2c8f518d2
Aidar <abdzhaid@cvut.cz>
2025-05-10 23:47:13 +0200

    Javadoc to AnimationInformation was added

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/Animation/AnimationInformation.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/Animation/AnimationInformation.java
@@ -1,13 +1,20 @@
 package cvut.cz.Animation;
 
 /**
- * Saves the information about one frame of animation
+ * Represents information about a single frame of animation.
+ * This record is used to define the source and target dimensions
+ * for rendering an animation frame on the screen.
  *
- * @param sourceX - Starting X coordinate (in pixels) in the source image from which the animation is created
- * @param sourceY - Starting Y coordinate (in pixels) in the source image from which the animation is created
- * @param sourceWidth - Width of the source image
- * @param sourceHeight - Height of the source image
- * @param targetWidth - Width of the final image on the screen
- * @param targetHeight - Height of the final image on the screen
+ * @param sourceX       The starting X coordinate (in pixels) in the source image from which the animation frame is extracted.
+ * @param sourceY       The starting Y coordinate (in pixels) in the source image from which the animation frame is extracted.
+ * @param sourceWidth   The width (in pixels) of the animation frame in the source image.
+ * @param sourceHeight  The height (in pixels) of the animation frame in the source image.
+ * @param targetWidth   The width (in pixels) of the animation frame when rendered on the screen.
+ * @param targetHeight  The height (in pixels) of the animation frame when rendered on the screen.
  */
-public record AnimationInformation(int sourceX, int sourceY, int sourceWidth, int sourceHeight, int targetWidth, int targetHeight) { }
+public record AnimationInformation(int sourceX,
+                                   int sourceY,
+                                   int sourceWidth,
+                                   int sourceHeight,
+                                   int targetWidth,
+                                   int targetHeight) { }
