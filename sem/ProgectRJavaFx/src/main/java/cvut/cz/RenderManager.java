60ccb09e7ab84abc479818f1db507e2558347872
Aidar <abdzhaid@cvut.cz>
2025-05-11 00:05:06 +0200

    Javadoc to RenderManager was added

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/RenderManager.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/RenderManager.java
@@ -3,7 +3,7 @@ package cvut.cz;
 import cvut.cz.GameSprite.GameSprite;
 import cvut.cz.Model.MainPlayerModel;
 import cvut.cz.Model.MapModel;
-import cvut.cz.characters.GunSight;
+import cvut.cz.characters.CrossHair;
 import cvut.cz.items.InventoryCell;
 import cvut.cz.items.Item;
 import javafx.beans.property.ObjectProperty;
@@ -21,29 +21,53 @@ import javafx.scene.text.Font;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
+/**
+ * Manages the rendering of game elements, including sprites, inventory, and UI components.
+ * This class handles drawing operations and updates the game screen based on the current game state.
+ */
 public class RenderManager{
     private static final Logger logger = Logger.getLogger(RenderManager.class.getName());
 
+    // HashMap to store images to draw. Keys are the image paths, values are the images.
     private final HashMap<String, Image> imagesToDraw = new HashMap<>();
 
+    // Background for the dialogue option in the dialogue panel when it is currently selected
     public static Background activeBackGround = new Background(new BackgroundFill(Color.ORANGE, null, null));
+
+    // Background for the dialogue option in the dialogue panel when it is not currently selected
     public static Background notActiveBackGround = new Background(new BackgroundFill(Color.BEIGE, null, null));
 
     private final MainApplication mainApp;
 
+    // Observable property to hold the current message
     private final ObjectProperty<String[]> observableCurrentMessage;
+    // Flag to indicate if there is a new message
     private boolean haveNewMessage;
 
+    /**
+     * Constructs a RenderManager with the specified main application instance.
+     *
+     * @param application The main application instance.
+     */
     public RenderManager(MainApplication application) {
         mainApp = application;
 
         observableCurrentMessage = new SimpleObjectProperty<>();
         observableCurrentMessage.setValue(MainPlayerModel.getMainPlayerModel().getCurrentMessage());
+
+        // if value of observableCurrentMessage changes, set haveNewMessage to true
         observableCurrentMessage.addListener((_, _, _) -> haveNewMessage = true);
+
         haveNewMessage = false;
+
     }
 
-
+    /**
+     * Draws a game sprite on the specified graphics context.
+     *
+     * @param sprite The game sprite to draw.
+     * @param gc The graphics context to draw on.
+     */
     private void drawSprite(GameSprite sprite, GraphicsContext gc) {
         Image image = imagesToDraw.get(String.valueOf(sprite.getGameSpriteSourceInformation().getPathImage()));
 
@@ -57,6 +81,11 @@ public class RenderManager{
         }
     }
 
+    /**
+     * Draws inventory-related labels on the specified graphics context.
+     *
+     * @param gc The graphics context to draw on.
+     */
     private void drawInventoryLabels(GraphicsContext gc){
         InventoryCell currentInventoryCell = MainPlayerModel.getMainPlayerModel().getInventory().getSelectedInventoryCell();
         Item currentItem = currentInventoryCell.getItem();
@@ -79,74 +108,97 @@ public class RenderManager{
         }
     }
 
+    /**
+     * Draws the inventory and its contents on the specified graphics context.
+     *
+     * @param gc The graphics context to draw on.
+     */
     private void drawInventory(GraphicsContext gc) {
         gc.setFill(Color.WHITE);
-
         gc.setFont(Font.loadFont(String.valueOf(mainApp.getPathToFont()), 35));
         gc.clearRect(0, 0, mainApp.getScreenWidth(), mainApp.getScreenHeight());
+
         drawSprite(MainPlayerModel.getMainPlayerModel().getInventory(), gc);
 
+        // Draws items in the inventory cells
         for (InventoryCell cell : MainPlayerModel.getMainPlayerModel().getInventory().getCells()) {
             if (cell.getItem() != null) {
                 drawSprite(cell.getItem(), gc);
                 gc.fillText(String.valueOf(cell.getItem().getAmount()), cell.getCoordinateX() + MainPlayerModel.getMainPlayerModel().getInventory().getInventoryCellGeneralInformation().amountLabelCoordinateXRelativeToCell(), cell.getCoordinateY() + MainPlayerModel.getMainPlayerModel().getInventory().getInventoryCellGeneralInformation().amountLabelCoordinateYRelativeToCell());
             }
         }
+        // Draws the pointer
         if (MainPlayerModel.getMainPlayerModel().getInventory().getPointer() != null)
             drawSprite(MainPlayerModel.getMainPlayerModel().getInventory().getPointer(), gc);
 
         drawInventoryLabels(gc);
     }
 
+    /**
+     * Updates the rendering of the game screen based on the current game state.
+     * This method handles drawing sprites, UI components, and managing game interactions.
+     */
     public void update() {
-            if (MainPlayerModel.getMainPlayerModel().getMainPlayer().isDead()){
-                mainApp.getGuiCreator().getGameOverScreen().setVisible(true);
-                mainApp.getStage().getScene().setCursor(Cursor.CROSSHAIR);
-                return;
-            }
+        // Draws game over screen
+        if (MainPlayerModel.getMainPlayerModel().getMainPlayer().isDead()){
+            mainApp.getGuiCreator().getGameOverScreen().setVisible(true);
+            mainApp.getStage().getScene().setCursor(Cursor.CROSSHAIR);
+            return;
+        }
 
-            if (mainApp.getGraphicsContexts().isEmpty()) {
-                logger.severe("Don't have Graphics Context");
-                return;
-            }
+        if (mainApp.getGraphicsContexts().isEmpty()) {
+            logger.severe("Don't have Graphics Context");
+            return;
+        }
 
-            if (mainApp.getGuiCreator().getHealthBar() != null)
-                mainApp.getGuiCreator().getHealthBar().setProgress((double) MainPlayerModel.getMainPlayerModel().getMainPlayer().getCharacterInformation().getCurrentHealth() / MainPlayerModel.getMainPlayerModel().getMainPlayer().getCharacterInformation().getMaxHealth());
 
-            mainApp.getGraphicsContexts().getFirst().clearRect(0, 0, mainApp.getScreenWidth(), mainApp.getScreenHeight());
-            drawDrawableSprites();
+        if (mainApp.getGuiCreator().getHealthBar() != null) {
+            int currentProgress = MainPlayerModel.getMainPlayerModel().getMainPlayer().getCharacterInformation().getCurrentHealth();
+            int maxProgress = MainPlayerModel.getMainPlayerModel().getMainPlayer().getCharacterInformation().getMaxHealth();
+            mainApp.getGuiCreator().getHealthBar().setProgress((double) currentProgress/ maxProgress );
+        }
+        mainApp.getGraphicsContexts().getFirst().clearRect(0, 0, mainApp.getScreenWidth(), mainApp.getScreenHeight());
+        drawDrawableSprites();
 
-            if (MainPlayerModel.getMainPlayerModel().isInMenu()){
-                mainApp.getGuiCreator().getInGameMenu().setVisible(true);
-                mainApp.getStage().getScene().setCursor(Cursor.CROSSHAIR);
-            }
-            else{
-                mainApp.getGuiCreator().getInGameMenu().setVisible(false);
-                mainApp.getStage().getScene().setCursor(Cursor.NONE);
-            }
+        // Draws the in-game menu
+        if (MainPlayerModel.getMainPlayerModel().isInMenu()){
+            mainApp.getGuiCreator().getInGameMenu().setVisible(true);
+            mainApp.getStage().getScene().setCursor(Cursor.CROSSHAIR);
+        }
+        else{
+            mainApp.getGuiCreator().getInGameMenu().setVisible(false);
+            mainApp.getStage().getScene().setCursor(Cursor.NONE);
+        }
+        // Draws the dialogue panel if the player is interacting with an NPC
+        if (MainPlayerModel.getMainPlayerModel().isInteracting()) {
+            drawDialogue();
+        }else{
+            mainApp.getGuiCreator().getDialoguePanel().setVisible(false);
+        }
 
-            if (MainPlayerModel.getMainPlayerModel().isInteracting()) {
-                drawDialogue();
-            }else{
-                mainApp.getGuiCreator().getDialoguePanel().setVisible(false);
-            }
-
+        if (mainApp.getGraphicsContexts().size() > 1) {
+            mainApp.getGraphicsContexts().get(1).clearRect(0, 0, mainApp.getScreenWidth(), mainApp.getScreenHeight());
             if (MainPlayerModel.getMainPlayerModel().getMainPlayer().isAiming)
-                drawGunSight();
+                drawCrossHair(mainApp.getGraphicsContexts().get(1));
 
+            // Draws the inventory
             if (MainPlayerModel.getMainPlayerModel().isInventoryActive()) {
                 if (mainApp.getGraphicsContexts().size() < 2) {
-                    logger.severe("Don't have Graphics Context");
                     return;
                 }
                 drawInventory(mainApp.getGraphicsContexts().get(1));
             }
-
-            if (!MainPlayerModel.getMainPlayerModel().getMainPlayer().isAiming && !MainPlayerModel.getMainPlayerModel().isInventoryActive() && mainApp.getGraphicsContexts().size() > 1)
-                mainApp.getGraphicsContexts().get(1).clearRect(0, 0, mainApp.getScreenWidth(), mainApp.getScreenHeight());
+        }
+        else{
+            logger.severe("Don't have Graphics Context for the inventory");
+        }
     }
 
+    /**
+     * Draws the dialogue panel and updates its content based on the current message.
+     */
     private void drawDialogue(){
+        // Sets the current message to the observable property. if message differs from the previous one, set haveNewMessage to true
         observableCurrentMessage.setValue(MainPlayerModel.getMainPlayerModel().getCurrentMessage());
         VBox dialoguePanel = mainApp.getGuiCreator().getDialoguePanel();
         dialoguePanel.setVisible(true);
@@ -154,6 +206,8 @@ public class RenderManager{
         if (haveNewMessage) {
             for (int i = 0; i < MainPlayerModel.getMainPlayerModel().getCurrentMessage().length; i++) {
                 String currentLine = MainPlayerModel.getMainPlayerModel().getCurrentMessage()[i];
+
+                // if there is not enough lines in the dialogue panel, create a new one
                 if (i >= dialoguePanel.getChildren().size()) {
                     Label dialogueLine = new Label();
                     dialogueLine.setMaxSize(mainApp.getScreenWidth(), mainApp.getScreenHeight() / 3);
@@ -166,6 +220,8 @@ public class RenderManager{
 
                 ((Label) dialoguePanel.getChildren().get(i)).setText(currentLine);
             }
+
+            // if there are more lines in the dialogue panel than in the current message, set text in the extra lines to empty line
             for (int i = MainPlayerModel.getMainPlayerModel().getCurrentMessage().length; i < dialoguePanel.getChildren().size(); i++){
                 ((Label) dialoguePanel.getChildren().get(i)).setText("");
             }
@@ -173,29 +229,32 @@ public class RenderManager{
             haveNewMessage = false;
         }
 
+        // Sets the background of the dialogue panel to not active
         for (int i = 0; i < dialoguePanel.getChildren().size(); i++) {
             ((Label) dialoguePanel.getChildren().get(i)).setBackground(notActiveBackGround);
         }
 
+        // Sets the background of the currently selected dialogue option to active
         if (MainPlayerModel.getMainPlayerModel().getCurrentNPCInteractingWith().isWaitingForAnswer()) {
             int idx = MainPlayerModel.getMainPlayerModel().getCurrentNPCInteractingWith().getCurrentPlayersAnswer();
             ((Label) dialoguePanel.getChildren().get(idx)).setBackground(activeBackGround);
         }
     }
 
-    private void drawGunSight(){
-        if (mainApp.getGraphicsContexts().size() < 2){
-            logger.severe("Don't have Graphics Context");
-            return;
-        }
-
-        mainApp.getGraphicsContexts().get(1).clearRect(0, 0, mainApp.getScreenWidth(), mainApp.getScreenHeight());
-
-        GunSight gunSight = MainPlayerModel.getMainPlayerModel().getCurrentGunSight();
-        drawSprite(gunSight, mainApp.getGraphicsContexts().get(1));
+    /**
+     * Draws the crosshair on the screen when the player is aiming.
+     *
+     * @param gc The graphics context to draw on.
+     */
+    private void drawCrossHair(GraphicsContext gc){
+        CrossHair crossHair = MainPlayerModel.getMainPlayerModel().getCurrentGunSight();
+        drawSprite(crossHair, gc);
     }
 
-    private void drawDrawableSprites(){
+    /**
+     * Draws all drawable sprites on the screen.
+     */
+    private void drawDrawableSprites() {
         GameSprite currentSprite;
         for (int i = 0; i < MapModel.getMapModel().getDrawableObjects().size(); i++) {
             currentSprite = MapModel.getMapModel().getDrawableObjects().get(i);
@@ -204,9 +263,13 @@ public class RenderManager{
 
             drawSprite(currentSprite, mainApp.getGraphicsContexts().getFirst());
         }
-
     }
 
+    /**
+     * Retrieves the map of images to draw.
+     *
+     * @return The map of image paths to images.
+     */
     public HashMap<String, Image> getImagesToDraw() { return imagesToDraw; }
 
 }
