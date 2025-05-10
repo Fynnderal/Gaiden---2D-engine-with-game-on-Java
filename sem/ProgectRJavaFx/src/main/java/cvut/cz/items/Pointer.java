7e5001865434e8668f0c07e981dc6c64dcf3213a
Aidar <abdzhaid@cvut.cz>
2025-05-11 00:04:56 +0200

    Javadoc to Pointer was added

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/items/Pointer.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/items/Pointer.java
@@ -5,7 +5,16 @@ import cvut.cz.GameSprite.GameSprite;
 import cvut.cz.GameSprite.GameSpriteRenderInformation;
 import cvut.cz.GameSprite.GameSpriteSourceInformation;
 
+/**
+ * Represents a Pointer in the inventory.
+ * This class is used to create a pointer object with specific source and render information.
+ */
 public class Pointer extends GameSprite {
+    /**
+     * Constructor for creating a Pointer object.
+     *
+     * @param gameSpriteSourceInformation The source information for the game sprite.
+     */
     public Pointer(GameSpriteSourceInformation gameSpriteSourceInformation){
         super(gameSpriteSourceInformation, new GameSpriteRenderInformation(0, 0, 0, 0, 0, 0));
     }
