32094e305c78c64c8356eb66f5f9774f738c73ba
Aidar <abdzhaid@cvut.cz>
2025-05-11 00:06:21 +0200

    Javadoc to Updatable was added

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/Model/Updatable.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/Model/Updatable.java
@@ -1,5 +1,9 @@
 package cvut.cz.Model;
 
+/**
+ * Updates the state of the implementing object.
+ * This method is intended to be called each frame to refresh or modify the object's state.
+ */
 public interface Updatable {
     void update();
 }
