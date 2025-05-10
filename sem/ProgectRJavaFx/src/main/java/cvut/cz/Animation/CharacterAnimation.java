48e4123403cc2263fade0b40cac71cc2b8ef1abb
Aidar <abdzhaid@cvut.cz>
2025-05-10 23:47:39 +0200

    Javadoc to CharacterAnimation was added

--- sem/ProgectRJavaFx/src/main/java/cvut/cz/Animation/CharacterAnimation.java
+++ sem/ProgectRJavaFx/src/main/java/cvut/cz/Animation/CharacterAnimation.java
@@ -1,16 +1,22 @@
 package cvut.cz.Animation;
 
 /**
- * Saves the animations for the game characters
+ * Keeps the animations for the game characters. Saves idle, movement, dying and receiving damage animation.
  */
 public class CharacterAnimation {
+    //Keeps animation state that was performed before current state
     public AnimationStates previousAnimationState;
     public AnimationStates currentAnimationState;
 
     //Saves the index of the current animation frame to draw
     public int currentAnimation;
 
-
+    /**
+     * Initializes the character animation with default values:
+     * - currentAnimationState is set to IdleDown
+     * - previousAnimationState is set to null
+     * - currentAnimation is set to 0 (the first frame)
+     */
     public CharacterAnimation(){
         previousAnimationState = null;
         currentAnimationState = AnimationStates.IdleDown;
